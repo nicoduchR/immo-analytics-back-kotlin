b016fbdabc03f5e504935203badadddf0e53c77d
Duchemann <nicolas.duchemann@redlab.io>
2021-06-23 21:16:33 +0200

    feat(district)/IMMO-17: change entity and fix cron

--- src/main/kotlin/com/gnirps/api/district/model/District.kt
+++ src/main/kotlin/com/gnirps/api/district/model/District.kt
@@ -1,15 +1,17 @@
 package com.gnirps.api.district.model
 
 import com.gnirps.api.config.properties.ProjectProperties
+import com.gnirps.api.department.model.Department
 import java.util.*
 import javax.persistence.*
 import javax.validation.constraints.NotEmpty
+import javax.validation.constraints.NotNull
 
 @Entity
 data class District(
     @Id
     @org.hibernate.annotations.Type(type = "org.hibernate.type.UUIDCharType")
-    @Column(columnDefinition = "VARCHAR(${ProjectProperties.UUID_SIZE})")
+    @Column(length = ProjectProperties.UUID_SIZE)
     val id: UUID,
 
     @Column(unique=true)
