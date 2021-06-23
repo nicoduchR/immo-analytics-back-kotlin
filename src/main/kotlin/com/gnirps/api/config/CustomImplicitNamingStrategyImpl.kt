98ecca55fe8bb10235eb7f61345c39c544a46fbe
Duchemann <nicolas.duchemann@redlab.io>
2021-06-23 21:15:24 +0200

    feat(hibernate)/IMMO-18: implement custom naming strategy

---
+++ src/main/kotlin/com/gnirps/api/config/CustomImplicitNamingStrategyImpl.kt
@@ -0,0 +1,27 @@
+package com.gnirps.api.config
+
+import org.hibernate.boot.model.naming.Identifier
+import org.hibernate.boot.model.naming.ImplicitJoinColumnNameSource
+import org.hibernate.boot.model.naming.ImplicitNamingStrategyJpaCompliantImpl
+
+/**
+ * Custom implementation of Hibernate implicit strategy
+ */
+class CustomImplicitNamingStrategyImpl : ImplicitNamingStrategyJpaCompliantImpl() {
+    override fun determineJoinColumnName(source: ImplicitJoinColumnNameSource): Identifier? {
+        val name: String = if (source.nature == ImplicitJoinColumnNameSource.Nature.ELEMENT_COLLECTION
+            || source.attributePath == null) {
+            (
+                    transformEntityName(source.entityNaming)
+                            + source.referencedColumnName.text.capitalize()
+                    )
+        } else {
+            (
+                    transformAttributePath(source.attributePath)
+                            + source.referencedColumnName.text.capitalize()
+                    )
+        }
+
+        return toIdentifier(name, source.buildingContext)
+    }
+}
